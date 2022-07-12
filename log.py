c77502daf396cd47d056ccc545c91e8150667102
BonnetPonta <rkthnw@gmail.com>
2022-07-12 23:01:28 +0900

    fix

--- log.py
+++ log.py
@@ -15,20 +15,18 @@ class Color:
     UNDERLINE = '\033[4m'
 
 
-def get_log(msg, datetime: bool = False, default_color: bool = True):
+def get_log(msg, datetime: bool = False, color: Color = Color.CYAN):
     """ Paramters:
         datetime`bool`: 日時を表示するか否か
-        default_color`bool`: カスタム色付きメッセージを使用するか
+        color`bool`: print色
     """
     res = ""
     if datetime:
         res += f"[{Color.GREEN}{dt.now()}{Color.RESET}] "
-    if default_color:
-        res += str(Color.CYAN)
-    res += f"{msg}{Color.RESET}"
+    res += f"{color}{msg}{Color.RESET}"
     return res
 
 
-def log(msg, datetime: bool = False, custom_color: bool = True) -> str:
+def log(msg, datetime: bool = False, color: Color = Color.CYAN) -> str:
     """メッセージ文字列をカスタマイズしてprintする"""
-    print(get_log(msg, datetime, custom_color))
+    print(get_log(msg, datetime, color))
