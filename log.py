89fff4c6361a1406bcd3257ff2cc0e4cab9aa5a6
BonnetPonta <rkthnw@gmail.com>
2022-07-12 22:14:28 +0900

    add

--- log.py
+++ log.py
@@ -2,17 +2,29 @@ from datetime import datetime
 
 
 class Color:
-    CYAN = '\033[36m'
+    BLACK = '\033[30m'
+    RED = '\033[31m'
     GREEN = '\033[32m'
+    YELLOW = '\033[33m'
+    BLUE = '\033[34m'
+    PURPLE = '\033[35m'
+    CYAN = '\033[36m'
+    WHITE = '\033[37m'
     RESET = '\033[0m'
+    BOLD = '\038[1m'
+    UNDERLINE = '\033[4m'
 
 
-class Print:
-    @staticmethod
-    def log(msg):
-        print(f"{Color.CYAN}{msg}{Color.RESET}")
+def log(msg):
+    """シアン色指定メッセージ"""
+    print(f"{Color.CYAN}{msg}{Color.RESET}")
 
-    @staticmethod
-    def debuglog(msg):
-        """日時付き"""
-        print(f"[{Color.GREEN}{datetime.now()}{Color.RESET}] {Color.CYAN}{msg}{Color.RESET}")
+
+def customLog(msg):
+    """色指定なしメッセージ。Colorクラスから選択"""
+    print(f"{msg}{Color.RESET}")
+
+
+def debugLog(msg):
+    """日時付きメッセージ"""
+    print(f"[{Color.GREEN}{datetime.now()}{Color.RESET}] {Color.CYAN}{msg}{Color.RESET}")
